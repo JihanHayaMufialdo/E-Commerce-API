d38f3cd87ba76acc373aae154e673c93367ce05a
JihanHayaMufialdo <jihannhayaa@gmail.com>
2025-09-18 14:19:49 +0700

    Initial setup with Node.js, Express, Prisma, PostgreSQL

---
+++ prisma/migrations/20250918071546_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" SERIAL NOT NULL,
+    "userName" TEXT NOT NULL,
+    "userEmail" TEXT NOT NULL,
+    "userPassword" TEXT NOT NULL,
+    "userRole" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_userEmail_key" ON "public"."users"("userEmail");
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("E-Commerce API running with Prisma 🚀");
+});
+
+module.exports = app;
